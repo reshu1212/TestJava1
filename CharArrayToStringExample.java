0ede942f7842c7c9197605c2bab614521a2f3f64
Reshu garg <reshu.garg@cytiva.com>
2022-01-22 14:54:39 +0530

    Commiting JavaCode

--- CharArrayToStringExample.java
+++ CharArrayToStringExample.java
@@ -3,7 +3,7 @@ public class CharArrayToStringExample
 public static void main(String args[])  
 {  
 //character array  
-char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'J', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't'};  
+char[] ch = {'R', 'e', 's', 'h', 'u', 'g', 'a', 'r ' , 'g'};  
 //constructor of the String class that parses char array as a parameter  
 String string = new String(ch);  
 //prints the string  
